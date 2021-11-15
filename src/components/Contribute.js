4d0604124ac3cf6e95898c6d97dc97b0608eff83
xyang3672 <xyang3672@bths.edu>
2021-11-15 17:55:11 -0600

    update

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -6,17 +6,22 @@ import { useState } from "react";
 const Contribute = ({setPage, currentStory}) => {
    
     const [text, setText] = useState(null);
-    
+    const [checkout, setCheckout] = useState(false);
     return (
         <div className = "contribute" >
             <p> Genre: {currentStory.genre}</p>
             <p>{currentStory.text}</p>
             <div className="input"> 
-                <textarea
+                {checkout?
+                    <textarea
                     type="text" 
                     id = "text"
                     onChange={e => setText(currentStory.text + "\n\n" + e.target.value)}>
                 </textarea>
+                    :
+                    <button onClick={()=> setCheckout(true)}>checkedout</button>
+                }
+                
                 <br/>
                 <button onClick={()=> setPage("available")}>Back</button>
                 <button onClick={()=> console.log(text)}> Submit </button>
